55dfb173a28110792fe2b4e845b822d17c0a31a2
김영훈 <joseph0926@naver.com>
2022-11-11 09:57:40 +0900

    2022.11.11

--- ch16/src/components/SimpleInput.js
+++ ch16/src/components/SimpleInput.js
@@ -1,27 +1,77 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
-  const enteredRef = useRef();
   const [enteredValue, setEnteredValue] = useState("");
+  const [enteredValueTouch, setEnteredValueTouch] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouch, setEnteredEmailTouch] = useState(false);
+
+  const enteredValueIsValid = enteredValue.trim() !== "";
+  const nameInputIsValid = !enteredValueIsValid && enteredValueTouch;
+
+  const enteredEmailIsValid = enteredEmail.trim() !== "" && enteredEmail.includes("@");
+  const emailInputIsValid = !enteredEmailIsValid && enteredEmailTouch;
+
+  useEffect(() => {
+    if (enteredValueIsValid && enteredEmailIsValid) {
+      setFormIsValid(true);
+    } else {
+      setFormIsValid(false);
+    }
+  }, [enteredValueIsValid, enteredEmailIsValid]);
+
   const inputHandler = (e) => {
     setEnteredValue(e.target.value);
+  };
+  const inputBlurHandler = (e) => {
+    setEnteredValueTouch(true);
+  };
+
+  const emailInputHandler = (e) => {
+    setEnteredEmail(e.target.value);
+  };
+  const emailBlurHandler = (e) => {
+    setEnteredEmailTouch(true);
   };
 
   const submitHandler = (e) => {
     e.preventDefault();
-    const enteredRefValue = enteredRef.current.value;
+
+    setEnteredValueTouch(true);
+    setEnteredEmailTouch(true);
+
+    if (!enteredValueIsValid) {
+      return;
+    }
+    if (!enteredEmailIsValid) {
+      return;
+    }
 
     setEnteredValue("");
+    setEnteredEmail("");
+    setEnteredValueTouch(false);
+    setEnteredEmailTouch(false);
   };
+
+  const nameInputClasses = nameInputIsValid ? "form-control invalid" : "form-control";
+  const emailInputClasses = enteredEmailIsValid ? "form-control invalid" : "form-control";
 
   return (
     <form onSubmit={submitHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={inputHandler} ref={enteredRef} value={enteredValue} />
+        <input type="text" id="name" onChange={inputHandler} onBlur={inputBlurHandler} value={enteredValue} />
+        {nameInputIsValid && <p className="error-text">이름이 빈칸일 수 없습니다.</p>}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your E-mail</label>
+        <input type="email" id="email" onChange={emailInputHandler} onBlur={emailBlurHandler} value={enteredEmail} />
+        {emailInputIsValid && <p className="error-text">이메일 양식을 맞춰주세요.</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
